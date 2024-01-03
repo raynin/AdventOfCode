0799948c0becd060248179f3bce7338cad1a44e7
raynin <ray.power1432@gmail.com>
2024-01-03 17:37:44 +0000

    corrections

--- AOC2023/Day1/day1.py
+++ AOC2023/Day1/day1.py
@@ -10,11 +10,16 @@ for line in lines:
     for i, c in enumerate(line):
         if c.isdigit():
             a.append(c)
-        for k, num in enumerate(['one','two','three','four','five','six','seven','eight','nine']):
-            if c[i:].startswith(num):
-                b.append(str(k))
-            
-    p1 += int(a[0]+a[-1])
-    print(b)
+            b.append(c)
+        else:
+            for k, num in enumerate(['one','two','three','four','five','six','seven','eight','nine']):
+                if line[i:].startswith(num):
+                    b.append(str(k + 1))
+    if len(a):
+        p1 += int(a[0]+a[-1])
+    if len(b):
+        p2 += int(b[0]+b[-1])
+
 
 print(p1)
+print(p2)
